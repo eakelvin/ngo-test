9775be1b5d62d9fe5960cfbfa35625bf1e38aefc
Kelvin Akaba <akabakelvin@gmail.com>
2024-09-11 17:06:26 +0000

    dark ui

--- components/forms/contact.tsx
+++ components/forms/contact.tsx
@@ -8,6 +8,7 @@ import { RxEnvelopeClosed } from 'react-icons/rx'
 import { IoCallOutline } from 'react-icons/io5'
 import toast from 'react-hot-toast'
 import axios from 'axios'
+import { Textarea } from '../ui/textarea'
 
 const Contact = () => {
     const [loading, setLoading] = useState<Boolean>(false);
@@ -45,7 +46,7 @@ const Contact = () => {
             </div>
             <div className="flex items-stretch justify-center mt-8">
                 <div className="grid md:grid-cols-3 gap-8">
-                    <div className="md:col-span-1 w-full shadow md:p-2 p-5 h-full">
+                    <div className="md:col-span-1 w-full dark:shadow-md shadow md:p-2 p-5 h-full">
                         <ul className="">
                             <li className='mb-4'>
                                 <div className='flex flex-row items-center'>
@@ -60,13 +61,11 @@ const Contact = () => {
                                     <p className="">
                                         We are available within 48 hours
                                     </p>
-                                    <p className="">
-                                        Phone: 0211110009
-                                    </p>
+                                    <p className="">Phone: 0211110009</p>
                                     <p className=" ">
                                         Email: essentialaid@gmail.com
                                     </p>
-                                    <p className=" text-gray-600 dark:text-slate-400">
+                                    <p className="">
                                         Address: EA Street, 1442 GR East Lane
                                     </p>
                                 </div>
@@ -77,19 +76,19 @@ const Contact = () => {
                                     <div className="flex h-10 w-10 items-center justify-center rounded-full bg-[#040D7F] text-gray-50">
                                         <RxEnvelopeClosed size={20} />
                                     </div>
-                                    <h3 className="ml-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                                    <h3 className="ml-2 text-lg font-medium leading-6">
                                         Send us a Message
                                     </h3>
                                 </div>
                                 <div className="my-4">
-                                    <p className="mb-2 text-gray-600 dark:text-slate-400">
+                                    <p className="mb-2">
                                         Fill out our form and we will contact you within 24 hours
                                     </p>
                                 </div>
                             </li>
                         </ul>
                     </div>
-                    <div className="md:col-span-2 w-full shadow card h-full max-w-6xl p-5 md:p-12">
+                    <div className="md:col-span-2 w-full dark:shadow-lg shadow card h-full max-w-6xl p-5 md:p-12">
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="mb-6">
                                 <div className="md:flex md:flex-row md:space-x-4 space-y-3 md:space-y-0">
@@ -116,19 +115,19 @@ const Contact = () => {
                                 </div>
                                 <div className="mx-0 mb-1 sm:mb-4 mt-3 md:mt-3">
                                     <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider"></label>
-                                    <textarea
+                                    <Textarea
                                         cols={30}
                                         rows={7}
                                         placeholder="Your Message"
-                                        className="text-base mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0"
+                                        className="text-base mb-2 shadow-md sm:mb-0"
                                         {...register("message")}
                                         required
                                     >
-                                    </textarea>
+                                    </Textarea>
                                 </div>
                             </div>
                             <div className="flex justify-end">
-                                <Button type='submit'>
+                                <Button variant={"default"} type='submit'>
                                     {loading ? <Spinner /> : 'Send Message'}
                                 </Button>
                             </div>
