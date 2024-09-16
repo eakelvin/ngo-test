5331f391922aae048d4b9a9878a3999e421b5b5a
Kelvin Akaba <akabakelvin@gmail.com>
2024-09-16 19:18:24 +0000

    csr

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import Donate from '@/components/forms/donate'
+// import Donate from '@/components/forms/donate'
+import dynamic from 'next/dynamic';
+
+const Donate = dynamic(
+    () => import("@/components/forms/donate"),
+    { ssr: false }
+);
 
 const page = () => {
     return (
