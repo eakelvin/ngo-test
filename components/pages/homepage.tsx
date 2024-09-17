202de8698ee6e4c997a509756df09225034dda92
Kelvin Akaba <akabakelvin@gmail.com>
2024-09-17 12:46:10 +0000

    community section

--- components/pages/homepage.tsx
+++ components/pages/homepage.tsx
@@ -44,7 +44,7 @@ const HomePage = () => {
 
                     <figure className="relative max-w-full transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 sm:hidden">
                         <img className="rounded-lg h-auto max-w-full" src="https://images.unsplash.com/photo-1701189991917-162ce50220e4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBvb3IlMjBjaGlsZHJlbiUyMGluJTIwYWZyaWNhfGVufDB8fDB8fHww" alt="image description" />
-                        <figcaption className="absolute px-4 text-2xl text-white font-extrabold bottom-6">
+                        <figcaption className="absolute px-4 text-2 text-white font-extrabold bottom-6">
                             <p>Wanna help children in Africa?</p>
                         </figcaption>
                     </figure>
@@ -143,6 +143,44 @@ const HomePage = () => {
                 <h1 className='text-xl md:text-2xl mb-6'>Fundraisers in Extreme Need</h1>
                 <SwiperCards />
             </div>
+
+            <section className=''>
+                <div className="mx-auto sm:px-6 lg:px-8">
+                    <div className="relative isolate overflow-hidden px-6 py-24 text-center sm:rounded-3xl sm:px-16">
+                        <h2 className="font-nudge-extrabold mx-auto max-w-2xl text-2xl md:text-3xl font-bold uppercase tracking-wide">
+                            Join our community now
+                        </h2>
+                        <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
+                            Join our community for donating and be a part of a positive change in the world
+                        </p>
+                        <div className="isolate mt-8 flex items-center justify-center -space-x-2 overflow-hidden">
+                            <img className="relative z-30 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/men/34.jpg" alt="" />
+                            <img className="relative z-20 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/women/2.jpg" alt="" />
+                            <img className="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/women/3.jpg" alt="" />
+                            <img className="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/men/4.jpg" alt="" />
+                            <span className="!ml-2 font-bold italic text-teal-500">Join these awesome members</span>
+                        </div>
+                        <div className='mt-5'>
+                            <p className='font-extrabold text-6xl'>120, 859+</p>
+                            <p className='mt-6'>people already joining</p>
+                        </div>
+                        <div className="flex items-center justify-center gap-x-6">
+                            <Button>Join Now</Button>
+                        </div>
+                        <svg viewBox="0 0 1024 1024"
+                            className="absolute left-1/2 top-1/2 -z-10 h-[72rem] w-[72rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+                            aria-hidden="true">
+                            <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7"></circle>
+                            <defs>
+                                <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+                                    <stop stop-color="#15b8a6"></stop>
+                                    <stop offset="1" stop-color="#15b8a6"></stop>
+                                </radialGradient>
+                            </defs>
+                        </svg>
+                    </div>
+                </div>
+            </section>
 
             <div className='mt-10 p-5'>
                 <h1 className='font-bold text-2xl md:text-3xl text-center mb-3'>
